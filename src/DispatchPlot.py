71706c9b99af67180c01ee94a9e012acd49961a8
Dylan McDowell <dylanjm@users.noreply.github.com>
2021-05-20 11:43:30 -0600

    Add initial plotting scheme

--- src/DispatchPlot.py
+++ src/DispatchPlot.py
@@ -6,6 +6,7 @@
 """
 import os
 import sys
+from itertools import groupby
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -15,12 +16,12 @@ from PluginBaseClasses.OutStreamPlotPlugin import PlotPlugin, InputTypes, InputD
 # Matplotlib Global Settings
 plt.rc("figure", figsize=(12, 8), titleweight='bold')
 plt.rc(
-    "axes",
-#     titlesize=25,
-    titleweight="bold",
-    labelsize=12,
-    axisbelow=True,
-    grid=True
+  "axes",
+  #titlesize=25,
+  titleweight="bold",
+  labelsize=12,
+  axisbelow=True,
+  grid=True
 )
 plt.rc("savefig", bbox="tight")
 plt.rc("legend", fontsize=12)
@@ -68,33 +69,25 @@ class DispatchPlot(PlotPlugin):
       self.raiseAnError(IOError, f'Source DataObject {self._sourceName} was not found in the Step!')
     self._source = src
 
-  def _dispatch_electro(dat, ax):
-    """
-    """
-    pass
-
-  def _dispatch_hydro(dat, ax):
-    """
-    """
-    pass
-
-  def _dispatch_storage(dat, ax):
-    """
-    """
-    pass
-
 
   def run(self):
     """
     """
     idx = pd.IndexSlice
     data = self._source.asDataset().to_dataframe()
-    data = data.loc[idx[0, :, :, 0]].reset_index()
+    data = data.loc[idx[0, :, 10, 0]].reset_index()
 
-    dispatch_vars = list(filter(lambda x: "Dispatch__" in x, data.columns))
+    dispatch_vars = filter(lambda x: "Dispatch__" in x, data.columns)
+    grouped_resources = groupby(dispatch_vars, lambda x: x.split('__')[-1])
+
     fig = plt.figure()
-    ax = fig.add_subplot(111)
-    for var in dispatch_vars:
-      ax.plot(data['Time'], data[var])
+    # TODO Add loop for Years and _ROM_Clusters
+    for i, (key, group) in enumerate(grouped_resources):
+      ax = fig.add_subplot(2,1,i+1)
+      for var in group:
+        # NOTE I don't think we can rely on 'Time' being the unique time-dependent variable.
+        ax.plot(data['Time'], data[var], label=var.replace('__', ' ').title())
+        ax.set_xlabel('Time')
+      ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
     fig.savefig("plot.png")
